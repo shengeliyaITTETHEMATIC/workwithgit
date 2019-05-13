9630ab6f8d4f5ee63d1cd46dad8bd92b76d23cfb
shengeliya <shengeliya_ag@it-thematic.ru>
2019-05-13 13:44:34 +0300

    add statement's workspace

--- project/polls/urls.py
+++ project/polls/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url('delete_statements', views.delete_statements, name='delete'),
+    url('delete_statement', views.delete_statement, name='delete'),
+    url('add_statements', views.change_statement, name='check'),
+    url('add_statement', views.add_statement, name='add'),
+    url('check_statement', views.check_statement, name='check'),
+    url('statement', views.check_statement, name='check'),
+    url('check_login', views.check_login, name='log'),
+    url(r'^$', views.login, name='change'),
 ]
--- project/polls/views.py
+++ project/polls/views.py
@@ -1,6 +1,64 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Statement
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.shortcuts import redirect
+from django.conf import settings
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def change_statement(request):
+    if not request.user.is_authenticated():
+        statements = Statement.objects.all()
+        return render(request, 'polls/change_statement.html', {'statements': statements})
+    else:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
+
+def add_statement(request):
+    name = request.POST.get('name')
+    if name != "":
+        data = Statement(name=name)
+        data.save()
+    statements = Statement.objects.all()
+    return render(request, 'polls/change_statement.html', {'statements': statements})
+
+
+def check_statement(request):
+    statements = Statement.objects.all()
+    return render(request, 'polls/check_statement.html', {'statements': statements})
+
+
+def delete_statements(request):
+    statements = Statement.objects.all()
+    return render(request, 'polls/delete_statement.html', {'statements': statements})
+
+
+def delete_statement(request):
+    id = request.POST.get('ID')
+    data = Statement.objects.get(id=id)
+    data.delete()
+    statements = Statement.objects.all()
+    return render(request, 'polls/delete_statement.html', {'statements': statements})
+
+
+def login(request):
+    return render(request, 'polls/login.html', {})
+
+
+def check_login(request):
+    log = request.POST.get('name')
+    pas = request.POST.get('pass')
+    user = authenticate(username=log, password=pas)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return render(request, 'polls/check_statement.html', {})
+        else:
+            return render(request, 'polls', {})
+    else:
+        return render(request, 'polls', {})
